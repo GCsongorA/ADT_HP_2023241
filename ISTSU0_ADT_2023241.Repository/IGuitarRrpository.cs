f934c9fdb14b308e6897ab659fd7e1c7e72d0e80
GCsongor <gorcsos.csongor@gmail.com>
2023-12-13 20:07:53 +0100

    Added IGuitarRepository

---
+++ ISTSU0_ADT_2023241.Repository/IGuitarRrpository.cs
@@ -0,0 +1,18 @@
+﻿using ISTSU0_ADT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTSU0_ADT_2023241.Repository
+{
+    public interface IGuitarRepository
+    {
+        public IQueryable<Guitar> GetAll();
+        public Task<Guitar?> GetOneAsync(Guid id);
+        public Task<Guitar?> UpdateAsync(Guid id, Guitar guitar);
+        public Task<Guitar?> DeleteAsync(Guid id);
+        public Task<Guitar> CreateAsync(Guitar guitar);
+    }
+}
