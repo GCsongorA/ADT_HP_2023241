ea484ad47b441658fcfb91dcd0351cc820d82582
GCsongor <gorcsos.csongor@gmail.com>
2023-12-13 20:16:38 +0100

    added IGuitarLogic and Guitarlogic

---
+++ ISTSU0_ADT_2023241.Logic/GuitarLogic.cs
@@ -0,0 +1,51 @@
+﻿using ISTSU0_ADT_2023241.Models;
+using ISTSU0_ADT_2023241.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTSU0_ADT_2023241.Logic
+{
+    internal class GuitarLogic: IGuitarLogic
+    {
+        private readonly IGuitarRepository guitarRepository;
+
+        public GuitarLogic(IGuitarRepository guitarRepository)
+        {
+            this.guitarRepository = guitarRepository;
+        }
+
+
+        #region CRUD
+        public async Task<Guitar> CreateAsync(Guitar guitar)
+        {
+            return await guitarRepository.CreateAsync(guitar);
+        }
+
+        public async Task<Guitar?> DeleteAsync(Guid id)
+        {
+            return await guitarRepository.DeleteAsync(id);
+        }
+
+        public IQueryable<Guitar> GetAll()
+        {
+            return guitarRepository.GetAll();
+        }
+
+        public async Task<Guitar?> GetOneAsync(Guid id)
+        {
+            return await guitarRepository.GetOneAsync(id);
+        }
+
+        public async Task<Guitar?> UpdateAsync(Guid id, Guitar guitar)
+        {
+            return await guitarRepository.UpdateAsync(id, guitar);
+        }
+
+
+
+        #endregion
+    }
+}
---
+++ ISTSU0_ADT_2023241.Logic/IGuitarLogic.cs
@@ -0,0 +1,18 @@
+﻿using ISTSU0_ADT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTSU0_ADT_2023241.Logic
+{
+    public interface IGuitarLogic
+    {
+        public IQueryable<Guitar> GetAll();
+        public Task<Guitar?> GetOneAsync(Guid id);
+        public Task<Guitar?> UpdateAsync(Guid id, Guitar guitar);
+        public Task<Guitar?> DeleteAsync(Guid id);
+        public Task<Guitar> CreateAsync(Guitar guitar);
+    }
+}
